278ecce205d5f6089fdaf4bb0d6acf753263eb45
Szczerybut Konwarszewski <szczerybut.konwarszewski@gmail.com>
2017-05-19 23:45:55 +0200

    Adding some changes to counter

--- Fullscreen counter/Form1.Designer.vb
+++ Fullscreen counter/Form1.Designer.vb
@@ -1,9 +1,9 @@
-﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
-Partial Class Form1
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class main_window
     Inherits System.Windows.Forms.Form
 
     'Form overrides dispose to clean up the component list.
-    <System.Diagnostics.DebuggerNonUserCode()> _
+    <System.Diagnostics.DebuggerNonUserCode()>
     Protected Overrides Sub Dispose(ByVal disposing As Boolean)
         Try
             If disposing AndAlso components IsNot Nothing Then
@@ -20,11 +20,41 @@ Partial Class Form1
     'NOTE: The following procedure is required by the Windows Form Designer
     'It can be modified using the Windows Form Designer.  
     'Do not modify it using the code editor.
-    <System.Diagnostics.DebuggerStepThrough()> _
+    <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
+        Me.components = New System.ComponentModel.Container()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.c_label = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'Timer1
+        '
+        Me.Timer1.Interval = 1000
+        '
+        'c_label
+        '
+        Me.c_label.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.c_label.Font = New System.Drawing.Font("Arial Black", 120.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(238, Byte))
+        Me.c_label.ForeColor = System.Drawing.SystemColors.Control
+        Me.c_label.Location = New System.Drawing.Point(0, 0)
+        Me.c_label.Name = "c_label"
+        Me.c_label.Size = New System.Drawing.Size(729, 385)
+        Me.c_label.TabIndex = 1
+        Me.c_label.Text = "00:00:00"
+        Me.c_label.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'main_window
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.Text = "Form1"
-    End Sub
+        Me.BackColor = System.Drawing.SystemColors.ActiveCaptionText
+        Me.ClientSize = New System.Drawing.Size(729, 385)
+        Me.Controls.Add(Me.c_label)
+        Me.Name = "main_window"
+        Me.Text = "Fullscreen counter"
+        Me.ResumeLayout(False)
 
+    End Sub
+    Friend WithEvents Timer1 As Timer
+    Friend WithEvents c_label As Label
 End Class
--- Fullscreen counter/Form1.vb
+++ Fullscreen counter/Form1.vb
@@ -1,3 +1,38 @@
-﻿Public Class Form1
+﻿Public Class main_window
+
+    ' DECLARING VARIABLES
+    Dim c_window_seconds = -1
+    Dim c_window_minutes = 0
+    Dim c_window_hours = 0
+
+    ' SETTING VISUAL PROPERTIES OF COUNTER
+
+    Private Sub main_window_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Me.FormBorderStyle = FormBorderStyle.None
+        Me.WindowState = FormWindowState.Maximized
+        Timer1.Start()
+
+        toCenter()
+
+    End Sub
+
+    Sub toCenter()
+        ' This windows sets position of c_window text box to center
+    End Sub
+
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+
+        Dim init_text As String = "0" & c_window_hours & ":" & "0" & c_window_minutes & ":" & c_window_seconds
+
+        c_window_seconds = c_window_seconds + 1
+
+        If c_window_seconds = 60 Then
+
+            c_window_seconds = 0
+            c_window_minutes += 1
+        End If
+        If c_window_seconds < 10 Then c_window_seconds = "0" & c_window_seconds
+        c_label.Text = init_text
+    End Sub
 
 End Class
